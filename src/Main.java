a2953a3df5d9ddac13ff98af4c0de6b23f32ccd2
dgon <fill3432@yandex.ru>
2022-08-16 11:57:07 +0300

    Зделал ДЗ номер 8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание1
+        String name ="Ivan";
+        String surname ="Ivanov";
+        String patronymic = "Ivanovich";
+        String fullName = surname + " "+ name +","+ patronymic;
+        System.out.println("ФИО сотрудника-"+ fullName);
+
+        //Задание2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета-"+fullName.toUpperCase());
+
+        //Задание3
+        fullName="Иванов Семен Семенович";
+        String replaceFullName=fullName.replace("ё","е");
+        System.out.println("Данные ФИО сотрудника-" + replaceFullName);
     }
 }
